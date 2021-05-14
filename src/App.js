74776b74723127303501594f81ee613a82af32a0
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-05-14 16:30:38 +0530

    First commit after cleanup process

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Let's build!</h1>
+      {/* Header */}
+      {/* Title + Drop-down menu */}
+
+      {/* Info Box - 1 */}
+      {/* Info Box - 2 */}
+      {/* Info Box - 3 */}
+
+      {/* Table - Country Cases */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
